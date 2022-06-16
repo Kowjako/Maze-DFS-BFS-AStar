1c5eefea1ac74df5ac12e30a0afd272405a3f608
Włodzimierz Kowjako <kowyako@yandex.ru>
2022-06-16 22:48:48 +0200

    Added global configuration class

---
+++ Maze-DFS-BFS/Services/MenuMode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Maze_DFS_BFS.Services
+{
+    public enum SearchMode
+    {
+        BFS = 1,
+        DFS = 2
+    }
+
+    public enum ConfigType
+    {
+        AssigningStart = 1,
+        AssigningFinish = 2,
+        DrawingBorders = 3
+    }
+
+    public static class MenuMode
+    {
+        public static bool IsStartPointAssigned { get; set; }
+        public static bool IsFinishPointAssigned { get; set; }
+        public static SearchMode SearchMode { get; set; } = SearchMode.BFS;
+        public static ConfigType ConfigType { get; set; }
+
+        public static Color StartColor { get; set; }
+        public static Color FinishColor { get; set; }
+    }
+}
