3f94678e990bb374b26a95578794e46ea8b529be
Wlodzimierz <kowyako@yandex.ru>
2022-09-28 09:06:42 +0200

    Test fix

--- Maze-DFS-BFS/DXPanel.cs
+++ Maze-DFS-BFS/DXPanel.cs
@@ -3,7 +3,7 @@
 namespace Maze_DFS_BFS
 {
     /// <summary>
-    /// Standardowy WinForms Panel z włączonym podwójnym buforowaniem zeby uniknac freezow podczas animacji
+    /// Standardowy WinForms Panel z włączonym podwójnym buforowaniem zeby uniknac freezow podczas animacji.
     /// </summary>
     public class DXPanel : Panel
     {
